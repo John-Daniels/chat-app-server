5880eaef5e93c53d1c01c0f87bf11d57c423cceb
John Daniels Adeyemi <adeyemijohndaniels@gmail.com>
2022-11-08 15:54:37 +0100

    added new feature & eslint

--- src/controllers/sockets/socket.controller.js
+++ src/controllers/sockets/socket.controller.js
@@ -1,4 +1,4 @@
-import { Server } from "socket.io"
+import { Server } from "socket.io";
 
 import {
   generateMessage,
@@ -6,74 +6,99 @@ import {
   getUser,
   getUsersInRoom,
   removeUser,
-} from "../users/user.controller.js"
+} from "../users/user.controller.js";
 
 export default function (server) {
   const io = new Server(server, {
     cors: {
       origin: "*",
     },
-  })
+  });
 
   io.on("connection", (client) => {
-    console.log("new client connection", client.id)
+    console.log("new client connection", client.id);
 
     client.on("join", (data) => {
-      const { room, username } = data
-      if (!room) return
+      const { room, username } = data;
+      if (!room) return;
 
-      console.log(data)
-      const user = addUser({ id: client.id, ...data })
-      client.join(room)
+      console.log(data);
+      const user = addUser({ id: client.id, ...data });
+      client.join(room);
 
       client.broadcast
         .to(user.room)
         .emit(
           "message-recieve",
           generateMessage("Admin", `${user.username} has joined!`)
-        )
+        );
 
       io.to(user.room).emit("roomData", {
         room,
         users: getUsersInRoom(client.id, room),
-      })
-      io.sockets.to(client.id).emit("userData", { username })
-    })
+      });
+      io.sockets.to(client.id).emit("userData", { username });
+    });
 
     client.on("message", (data) => {
-      const user = getUser(client.id)
-      console.log(data)
-
-      client.broadcast.to(user.room).emit("message-recieve", data)
-    })
-
-    client.on("type", (data) => {
-      const user = getUser(client.id)
-      console.log(user)
-      client.broadcast.to(user.room).emit("typeing", data)
-    })
-
-    client.on("disconnect", () => {
-      console.log("client disconnect", client.id)
-
-      const user = getUser(client.id)
-      console.log("removed", user)
+      const user = getUser(client.id);
+      console.log(data);
 
       if (user?.room) {
-        const { room } = user
-        io.broadcast
-          ?.to(room)
-          .emit(
-            "message-recieve",
-            generateMessage("Admin", `${user.username} has left!`)
-          )
-        io.to(user.room).emit("roomData", {
+        client.broadcast.to(user.room).emit("message-recieve", data);
+      }
+    });
+
+    client.on("type", (data) => {
+      const user = getUser(client.id);
+      console.log(user);
+      if (user?.room) {
+        client.broadcast.to(user.room).emit("typeing", data);
+      }
+    });
+
+ client.on("logout", () => {
+      console.log("client disconnect", client.id);
+
+      const user = getUser(client.id);
+      console.log("removed", user);
+
+      if (user?.room) {
+        const { room } = user;
+        io.to(room).emit(
+          "message-recieve",
+          generateMessage("Admin", `${user.username} has left!`)
+        );
+        io.to(room).emit("roomData", {
           room,
           users: getUsersInRoom(room),
-        })
+        });
       }
 
-      removeUser(client.id)
-    })
-  })
+      removeUser(client.id);
+    });
+
+    client.on("disconnect", () => {
+      console.log("client disconnect", client.id);
+
+      const user = getUser(client.id);
+      console.log("removed", user);
+
+      if (user?.room) {
+        const { room } = user;
+        io.to(room).emit(
+          "message-recieve",
+          generateMessage("Admin", `${user.username} has left!`)
+        );
+        io.to(room).emit("roomData", {
+          room,
+          users: getUsersInRoom(room),
+        });
+      }
+
+      removeUser(client.id);
+    });
+
+
+  });
 }
--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -1,28 +1,28 @@
 // let onlineUsers = new Set()
-let users = []
+let users = [];
 
 export const addUser = (user) => {
-  const { username, room } = user
-  if (!username || !room) return
+  const { username, room } = user;
+  if (!username || !room) return;
 
-  users.push({ ...user, room: room.trim() })
-  return user
-}
+  users.push({ ...user, room: room.trim() });
+  return user;
+};
 
 export const removeUser = (id) => {
-  users = users.filter((user) => user?.id !== id)
-  return users
-}
+  users = users.filter((user) => user?.id !== id);
+  return users;
+};
 
 export const getUsersInRoom = (id, room) => {
-  const usersInRoom = users.filter((user) => user?.room == room)
+  const usersInRoom = users.filter((user) => user?.room == room);
   // return usersInRoom.filter((u) => u.id !== id)
-  return usersInRoom
-}
+  return usersInRoom;
+};
 export const generateMessage = (username, message) => ({
   username,
   message,
-  time: "",
-})
+  time: new Date().toDateString(),
+});
 
-export const getUser = (id) => users.find((user) => user.id == id)
+export const getUser = (id) => users.find((user) => user.id == id);
